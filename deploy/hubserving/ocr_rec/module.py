19a6c7a1f085fee108f3b422aba4c910c0247561
caiiiac <caiiiac@163.com>
2023-05-26 11:00:19 +0800

    修改ocr_rec

--- deploy/hubserving/ocr_rec/module.py
+++ deploy/hubserving/ocr_rec/module.py
@@ -20,31 +20,37 @@ import os
 import sys
 sys.path.insert(0, ".")
 import copy
-import paddlehub
-from paddlehub.common.logger import logger
+import gc
+
+import time
+import logging
+from ppocr.utils.logging import get_logger
 from paddlehub.module.module import moduleinfo, runnable, serving
 import cv2
+import numpy as np
 import paddlehub as hub
 
 from tools.infer.utility import base64_to_cv2
-from tools.infer.predict_rec import TextRecognizer
+from tools.infer.predict_system import TextSystem
 from tools.infer.utility import parse_args
-from deploy.hubserving.ocr_rec.params import read_params
-
+from deploy.hubserving.ocr_system.params import read_params
+logger = get_logger()
 
 @moduleinfo(
     name="ocr_rec",
     version="1.0.0",
-    summary="ocr recognition service",
+    summary="ocr rec service",
     author="paddle-dev",
     author_email="paddle-dev@baidu.com",
-    type="cv/text_recognition")
+    type="cv/PP-OCR_system")
 class OCRRec(hub.Module):
     def _initialize(self, use_gpu=False, enable_mkldnn=False):
         """
         initialize with the necessary elements
         """
         cfg = self.merge_configs()
+        if not cfg.show_log:
+            logger.setLevel(logging.INFO)
 
         cfg.use_gpu = use_gpu
         if use_gpu:
@@ -61,7 +67,7 @@ class OCRRec(hub.Module):
         cfg.ir_optim = True
         cfg.enable_mkldnn = enable_mkldnn
 
-        self.text_recognizer = TextRecognizer(cfg)
+        self.text_sys = TextSystem(cfg)
 
     def merge_configs(self, ):
         # deafult cfg
@@ -89,14 +95,14 @@ class OCRRec(hub.Module):
             images.append(img)
         return images
 
-    def predict(self, images=[], paths=[]):
+    def predict(self, images=[], paths=[], ids=[], locate=[]):
         """
-        Get the text box in the predicted images.
+        Get the chinese texts in the predicted images.
         Args:
             images (list(numpy.ndarray)): images data, shape of each is [H, W, C]. If images not paths
             paths (list[str]): The paths of images. If paths not images
         Returns:
-            res (list): The result of text detection box and save path of images.
+            res (list): The result of chinese texts and save path of images.
         """
 
         if images != [] and isinstance(images, list) and paths == []:
@@ -108,44 +114,72 @@ class OCRRec(hub.Module):
 
         assert predicted_data != [], "There is not any image to be predicted. Please check the input data."
 
-        img_list = []
-        for img in predicted_data:
+        all_results = []
+        for index, img in enumerate(predicted_data):
             if img is None:
+                logger.info("error in loading image")
+                all_results.append([])
                 continue
-            img_list.append(img)
+            starttime = time.time()
+            length = len(locate)
+            # locate 定位坐标(高起始,高结束,宽起始,宽结束) & 小于图片分辨率
+            # tt = length == 4 and locate[0] < locate[1] and locate[2] < locate[3] and locate[1] <= img.shape[0] and locate[3] <= img.shape[1]
+            # all_results.append({'locate': locate, 'shape': img.shape, 'tt': tt})
+            if length == 4 and locate[0] < locate[1] and locate[2] < locate[3] and locate[1] <= img.shape[0] and locate[3] <= img.shape[1]:
+                img = img[locate[0]:locate[1],locate[2]:locate[3]]
+            elif length > 0 and length < 4:
+                raise TypeError("The locate data is inconsistent with expectations.")
+            dt_boxes, rec_res, _ = self.text_sys(img)
+            elapse = time.time() - starttime
+            logger.info("Predict time: {}".format(elapse))
 
-        rec_res_final = []
-        try:
-            rec_res, predict_time = self.text_recognizer(img_list)
-            for dno in range(len(rec_res)):
-                text, score = rec_res[dno]
-                rec_res_final.append({
-                    'text': text,
-                    'confidence': float(score),
-                })
-        except Exception as e:
-            print(e)
-            return [[]]
+            dt_num = len(dt_boxes)
+            rec_res_final = []
 
-        return [rec_res_final]
+            for dno in range(dt_num):
+                text, _ = rec_res[dno]
+                # rec_res_final.append({
+                #     'text': text,
+                #     'confidence': float(score),
+                #     'text_region': dt_boxes[dno].astype(np.int_).tolist()
+                # })
+                rec_res_final.append(text)
+            all_results.append({
+                'id': ids[index],
+                'text':rec_res_final
+            })
+
+        # 手动释放内存
+        del predicted_data
+        gc.collect()
+        return all_results
 
     @serving
-    def serving_method(self, images, **kwargs):
+    def serving_method(self, images, type, ids, locate=[], **kwargs):
         """
         Run as a service.
         """
-        images_decode = [base64_to_cv2(image) for image in images]
-        results = self.predict(images_decode, **kwargs)
+        results = ""
+        if len(images) != len(ids):
+            raise TypeError("The input ids is inconsistent with expectations.")
+        
+        if(type == "image"):
+            images_decode = [base64_to_cv2(image) for image in images]
+            results = self.predict(images=images_decode, ids=ids, locate=locate, **kwargs)
+        elif(type == "path"):
+            results = self.predict(paths=images, ids=ids, locate=locate)
+        else:
+            raise TypeError("The input type is inconsistent with expectations.")
         return results
+    
 
 
 if __name__ == '__main__':
     ocr = OCRRec()
     ocr._initialize()
     image_path = [
-        './doc/imgs_words/ch/word_1.jpg',
-        './doc/imgs_words/ch/word_2.jpg',
-        './doc/imgs_words/ch/word_3.jpg',
+        './doc/imgs/11.jpg',
+        './doc/imgs/12.jpg',
     ]
     res = ocr.predict(paths=image_path)
     print(res)
